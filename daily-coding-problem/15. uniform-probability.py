9d5feda2f11bb955cb7647f05e68deb2ec39ab81
Sa Phyo Thu Htet <phyothuhtet.ai@gmail.com>
2022-04-20 15:48:15 +0630

    Create 15. uniform-probability.py

---
+++ daily-coding-problem/15. uniform-probability.py
@@ -0,0 +1,26 @@
+Medium
+
+'''
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Facebook.
+
+Given a stream of elements too large to store in memory, pick a random element from the stream with uniform probability.
+'''
+
+Solution:
+
+"""
+Uniform Probability ဆိုတာ ဘာလဲဆိုတာ ရှာကြည့်တယ်။
+
+Uniform Probability
+====================
+In statistics, uniform distribution is a term used to describe a form of probability distribution where every possible outcome has an equal likelihood of happening.
+The probability is constant since each variable has equal chances of being the outcome.
+
+Ref: https://corporatefinanceinstitute.com/resources/knowledge/other/uniform-distribution/
+
+
+ပထမဦးဆုံးကတော့  ဒီproblem ကို မသိဘူး။ မေးခွန်းကိုကော နားမလည်ဘူး။
+
+"""
